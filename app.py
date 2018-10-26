3c37ac87fd324ffe34b0ea6239f5f08616ee02fb
Cnels1986 <CNels1986@gmail.com>
2018-10-26 10:46:29 -0400

    Registration and login partly working. Access to pages are restricted when user is not logged in

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template, request, jsonify, make_response, json, session, flash
+from flask import Flask, render_template, request, jsonify, make_response, json, session, flash, redirect, url_for
 from pusher import pusher
 from flaskext.mysql import MySQL
 from passlib.hash import sha256_crypt
+from MySQLdb import escape_string as thwart
+from functools import wraps
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 mysql = MySQL()
 
 pusher = pusher_client = pusher.Pusher(
@@ -25,11 +29,31 @@ mysql.init_app(app)
 conn = mysql.connect()
 cursor = conn.cursor()
 
+# function checks the session loggin in status to see if user is logged in, if not it redirects them to the login page
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash("You need to login first")
+            # flash("This is a test...")
+            return redirect(url_for('login'))
+    return wrap
+
 #####################
 
 @app.route('/')
+@login_required
 def index():
     return render_template('index.html')
+
+#####################
+
+@app.route('/dashboard')
+@login_required
+def dash():
+    return render_template('dashboard.html')
 
 #####################
 
@@ -43,20 +67,23 @@ def register():
         name = request.form["name"]
 
         # Queries database to see if the username already exists, they must be unique
-        cursor.execute("SELECT * FROM Users WHERE userName = '{}'".format(username))
+        cursor.execute("SELECT * FROM Users WHERE userName = (%s)", thwart(username))
         user = cursor.fetchone();
 
         # user name does not exist within the database, will now add the information to the Users table
         if user == None:
             # adds new user to Users table
-            cursor.execute("INSERT INTO Users(userName, password) VALUES('{}','{}')".format(username, password))
-            conn.commit()
-
+            cursor.execute("INSERT INTO Users(userName, password) VALUES(%s, %s)", (thwart(username), thwart(password)))
             cursor.execute("SELECT * FROM Users WHERE userName = '{}'".format(username))
             userID = cursor.fetchone();
-
-            cursor.execute("INSERT INTO Stats(user_id, name, wins, losses, winStreak, lossStreak) VALUES('{}','{}','{}','{}','{}','{}')".format(userID[0], name, 0, 0, 0, 0))
+            cursor.execute("INSERT INTO Stats(user_id, name, wins, losses, winStreak, lossStreak) VALUES(%s, %s, 0,0,0,0)", (int(userID[0]), thwart(name)))
             conn.commit()
+            conn.close()
+            # sets the session so user is now logged in to the app
+            session['logged_in'] = True
+            session['username'] = username
+            flash("Thank you for registering")
+            return redirect(url_for('dash'))
         else:
             error = "Username already exists"
 
@@ -74,7 +101,7 @@ def login():
         username = request.form["userName"]
         password = request.form["password"]
 
-        cursor.execute("SELECT * FROM Users WHERE userName = '{}'".format(username))
+        cursor.execute("SELECT * FROM Users WHERE userName = (%s)", thwart(username))
         # retrieves hashed password from the Users table
         user = cursor.fetchone();
 
@@ -84,6 +111,10 @@ def login():
         else:
             if sha256_crypt.verify(request.form["password"], user[2]):
                 print("Passwords match")
+                session['logged_in'] = True
+                session['username'] = username
+                flash("You are now logged in")
+                return redirect(url_for('dash'))
             else:
                 errorPass = "Incorrect password"
 
@@ -114,6 +145,16 @@ def pusher_authentication():
     return json.dumps(auth)
 
 #####################
+
+@app.route('/logout')
+@login_required
+def logout():
+    session.clear()
+    flash("You are now logged out")
+    return redirect(url_for('login'))
+
+#####################
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
